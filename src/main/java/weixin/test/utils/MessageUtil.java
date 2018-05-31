d9691b25b1223f7480216ed16e94faeb59051598
libaoxin <baoxin.li@youyi-tech.com>
2018-05-31 10:12:05 +0800

    1111

---
+++ src/main/java/weixin/test/utils/MessageUtil.java
@@ -0,0 +1,15 @@
+package weixin.test.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MessageUtil {
+	
+	public static Map<String,String> xmlToMap(HttpServletRequest req){
+		Map<String,String> map=new HashMap<String,String>();
+		return map;
+	}
+
+}
